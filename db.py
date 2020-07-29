e846f1d0731f46a5c605387573bff90c7adc1b2c
tikochinsky <tt@sem-wolf.org>
2020-07-29 13:10:13 +0300

    beginning of index

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import csv
 import json
 import os
 import my_utils
+from BTrees.OOBTree import OOBTree
 
 
 @dataclass_json
@@ -12,16 +13,44 @@ class DBTable(DBTable):
     fields: List[DBField]
     key_field_name: str
 
+    def __init__(self, name, fields, key_field_name):
+        self.btree_index_info = {}
+        self.hash_index_info = {}
+        self.name = name
+        self.fields = fields
+        self.key_field_name = key_field_name
+
+        self.create_index(key_field_name, index_type="hash")
+
     def get_fields_names(self) -> List[str]:
         return [x.name for x in self.fields]
 
     def get_index_of_field(self, field: str) -> int:
         return self.get_fields_names().index(field)
 
+    def insert_into_index(self, record: List[any], path: str, line: int) -> None:
+        for i, field in enumerate(self.get_fields_names()):
+            if self.hash_index_info.get(field):
+                self.hash_index_info[field][record[i]] = {
+                    "path": path,
+                    "line": line
+                }
+
+            if self.btree_index_info.get(field):
+                self.hash_index_info[field].insert((record[i], {
+                    "path": path,
+                    "line": line
+                }))
+
+    def delete_from_index(self, record: List[any], path: str, line: int) -> None:
+        for i, field in enumerate(self.get_fields_names()):
+            if self.hash_index_info.get(field):
+                del self.hash_index_info[field][record[i]]
+
+            if self.btree_index_info.get(field):
+                self.hash_index_info[field].pop(record[i], None)
+
     def count(self) -> int:
-        # with open("db_files/table_info.json", "r") as json_file:
-        #     json_data = json.load(json_file)
-        #     return json_data[self.name]["count"]
         return DataBase.table_info[self.name]["count"]
 
     def insert_record(self, values: Dict[str, Any]) -> None:
@@ -40,7 +69,7 @@ class DBTable(DBTable):
         for name in self.get_fields_names():
             record_to_insert.append(values.get(name))
 
-        with open(f"db_files/{self.name}.csv", "r", newline="") as csv_file:
+        with open(f"{DB_ROOT}/{self.name}.csv", "r", newline="") as csv_file:
             csv_reader = csv.reader(csv_file)
 
             rows = []
@@ -56,18 +85,21 @@ class DBTable(DBTable):
                 if not record:
                     rows[i] = record_to_insert
                     inserted = True
+                    self.insert_into_index(record_to_insert, f"{DB_ROOT}/{self.name}.csv", i)
                     break
             if not inserted:
                 rows.append(record_to_insert)
+                self.insert_into_index(record_to_insert, f"{DB_ROOT}/{self.name}.csv", len(rows))
 
-        with open(f"db_files/{self.name}.csv", "w", newline="") as csv_file:
+        with open(f"{DB_ROOT}/{self.name}.csv", "w", newline="") as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerows(rows)
 
         DataBase.table_info[self.name]["count"] += 1
 
+
     def delete_record(self, key: Any) -> None:
-        with open(f"db_files/{self.name}.csv", "r") as csv_file:
+        with open(f"{DB_ROOT}/{self.name}.csv", "r") as csv_file:
             csv_reader = csv.reader(csv_file)
             index_of_key = self.get_index_of_field(self.key_field_name)
 
@@ -83,7 +115,7 @@ class DBTable(DBTable):
             if not found:
                 raise ValueError("Key not found")
 
-        with open(f"db_files/{self.name}.csv", "w", newline="") as csv_file:
+        with open(f"{DB_ROOT}/{self.name}.csv", "w", newline="") as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerows(clean_rows)
 
@@ -91,7 +123,7 @@ class DBTable(DBTable):
 
     def delete_records(self, criteria: List[SelectionCriteria]) -> None:
         num_of_removed = 0
-        with open(f"db_files/{self.name}.csv", "r") as csv_file:
+        with open(f"{DB_ROOT}/{self.name}.csv", "r") as csv_file:
             csv_reader = csv.reader(csv_file)
 
             clean_rows = [next(csv_reader)]
@@ -114,14 +146,14 @@ class DBTable(DBTable):
                     clean_rows.append([])
                     num_of_removed += 1
 
-        with open(f"db_files/{self.name}.csv", "w", newline="") as csv_file:
+        with open(f"{DB_ROOT}/{self.name}.csv", "w", newline="") as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerows(clean_rows)
 
         DataBase.table_info[self.name]["count"] -= num_of_removed
 
     def get_record(self, key: Any) -> Dict[str, Any]:
-        with open(f"db_files/{self.name}.csv", "r") as csv_file:
+        with open(f"{DB_ROOT}/{self.name}.csv", "r") as csv_file:
             csv_reader = csv.reader(csv_file)
 
             index_of_key = self.get_index_of_field(self.key_field_name)
@@ -134,19 +166,21 @@ class DBTable(DBTable):
         raise ValueError("The record doesn't exist in the table")
 
     def update_record(self, key: Any, values: Dict[str, Any]) -> None:
-        with open(f"db_files/{self.name}.csv", "r") as csv_file:
+        with open(f"{DB_ROOT}/{self.name}.csv", "r") as csv_file:
             csv_reader = csv.reader(csv_file)
-
-            index_of_key = self.get_index_of_field(self.key_field_name)
-            clean_rows = [record for record in csv_reader if record and record[index_of_key] != str(key)]
-            # keep the original --------------------------------------------------!!!!!!!!!
-            record_to_insert = []
-            for name in self.get_fields_names():
-                record_to_insert.append(values.get(name))
-            record_to_insert[self.get_index_of_field(self.key_field_name)] = key
-            clean_rows.append(record_to_insert)
-
-        with open(f"db_files/{self.name}.csv", "w", newline="") as csv_file:
+            clean_rows = []
+            key_index = self.get_index_of_field(self.key_field_name)
+            updated = False
+            for record in csv_reader:
+                if record and record[key_index] == str(key):
+                    updated = True
+                    for name in self.get_fields_names():
+                        if name in values.keys():
+                            record[self.get_index_of_field(name)] = values.get(name)
+                clean_rows.append(record)
+            if not updated:
+                raise ValueError("The record not exist")
+        with open(f"{DB_ROOT}/{self.name}.csv", "w", newline="") as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerows(clean_rows)
 
@@ -154,7 +188,7 @@ class DBTable(DBTable):
             -> List[Dict[str, Any]]:
         result = []
 
-        with open(f"db_files/{self.name}.csv", "r") as csv_file:
+        with open(f"{DB_ROOT}/{self.name}.csv", "r") as csv_file:
             csv_reader = csv.reader(csv_file)
             next(csv_reader)
             for record in csv_reader:
@@ -175,8 +209,29 @@ class DBTable(DBTable):
 
         return result
 
-    def create_index(self, field_to_index: str) -> None:
-        raise NotImplementedError
+    def create_index(self, field_to_index: str, index_type="btree") -> None:
+        index_of_field = self.get_index_of_field(field_to_index)
+        with open(f"{DB_ROOT}/{self.name}.csv", "r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            index_values = {}
+            next(csv_reader)
+            for record in csv_reader:
+                if record:
+                    index_values[record[index_of_field]] = {
+                        "path": "{DB_ROOT}/{self.name}.csv",
+                        "line": csv_reader.line_num
+                    }
+
+        if index_type == "btree":
+            btree_index = OOBTree()
+            btree_index.update(index_values)
+            self.btree_index_info[field_to_index] = btree_index
+
+        if index_type == "hash":
+            self.hash_index_info[field_to_index] = index_values
+
+        else:
+            raise ValueError("No such index type")
 
 
 @dataclass_json
@@ -184,6 +239,9 @@ class DBTable(DBTable):
 class DataBase(DataBase):
     def __init__(self):
         try:
+            if not os.path.exists(DB_ROOT):
+                DB_ROOT.mkdir(parents=True, exist_ok=True)
+
             with open("table_info.json", "r") as json_file:
                 DataBase.table_info = json.load(json_file)
 
@@ -202,10 +260,9 @@ class DataBase(DataBase):
         if key_field_name not in fields_names:
             raise ValueError("Key isn't in fields names")
 
-        with open(f"db_files/{table_name}.csv", "w", newline="") as csv_file:
+        with open(f"{DB_ROOT}/{table_name}.csv", "w", newline="") as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(fields_names)
-        # DataBase.__NUM_TABLE__ += 1
 
         if table_name in DataBase.table_info.keys():
             raise ValueError("Table already exists")
@@ -237,7 +294,7 @@ class DataBase(DataBase):
     def delete_table(self, table_name: str) -> None:
         try:
             del DataBase.table_info[table_name]
-            os.remove(f"db_files/{table_name}.csv")
+            os.remove(f"{DB_ROOT}/{table_name}.csv")
 
         except KeyError:
             raise ValueError("Table does not exist")
@@ -256,3 +313,9 @@ class DataBase(DataBase):
     def __del__(self):
         with open("table_info.json", "w") as json_file:
             json.dump(DataBase.table_info, json_file)
+
+
+# TODO:
+# divide tables to files
+# maintain index and change our queries to use indexes
+# joins
